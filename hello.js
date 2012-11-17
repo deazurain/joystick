d41baf722249175f0dbc02b5bf9fd299643cbcf0
Mick van Gelderen <mickfreeze@gmail.com>
2012-11-17 21:14:13 +0100

    removed old stuff

--- hello.js
+++
@@ -1,9 +0,0 @@
-ioctl = require('./build/Release/ioctl');
-fs = require('fs');
-
-fd = fs.openSync('/dev/input/js0', 'r');
-
-var JSIOCGBUTTONS = 0x80016A12;
-var buf = new Buffer(4);
-console.log(ioctl.ioctl(fd, JSIOCGBUTTONS, buf));
-console.log(buf);
